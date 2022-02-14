5edbe1b2ea2d24180aa39690c976770779c1cdaa
Matheus Alfaia <91610761+Matheus-Alfaia@users.noreply.github.com>
2022-02-14 15:00:57 -0400

    Add files via upload

---
+++ inicial.php
@@ -0,0 +1,39 @@
+<?php
+		include "cabecalho.php";
+		include "nav.php";
+?>	
+<center><img src="img/IMG-20220206-WA0012.jpg" width="60%" height="545"></center>
+<!-- <div class="col-sm-12">	
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <center><img class="d-block img" src="img/90f1b0d3f5a85b5525fb4b7b3a5ec162.jpg" alt="IMG"></center>
+    </div>
+    <div class="carousel-item">
+       <center><img class="d-block img1" src="img/images(48).jpg" alt="IMG"></center>
+    </div>
+    <div class="carousel-item">
+       <center><img class="d-block img2" src="img/PicsArt_11-14-08.08.18.jpg" alt="IMG"></center>
+    </div>
+  </div> -->
+</div>
+</div>
+  <script src="js/jquery-3.2.1.slim.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+  <!--<?php
+  include_once "conn/conexao.php";
+  include "organizador.php";
+  $local = $_SERVER['PHP_SELF'];
+  $env = visu($conexao, $local);
+  $count = contar($conexao);
+  foreach($count as $linha){
+      echo '<td>'.$linha["local"].'</td><br>';
+      echo '<td>'.$linha["contador"].'</td>';
+      echo '</tr>';
+    }
+  ?> --> 
+  
+  <?php
+  	include "rodape.php";
+  	?>
